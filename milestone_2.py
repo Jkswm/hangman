df7e5526a8007056401ef5fd07f18ece1f738eb6
Jkswm <beatrixawuah@outlook.com>
2023-10-29 16:52:33 +0000

    Milestone_2.py 2nd Update

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,15 @@
+import random
+def my_random_choice(seq):
+    if not seq:
+        raise ValueError ("Sequence cannot be empty)")
+    return seq[random.randint(0, len(seq) - 1)]
+
 word_list = ["grapes","strawberries","apples","pears","kiwis"]
 print(word_list)
+word = my_random_choice(word_list)
+print(word)
+guess = input("Please enter a single letter: ")
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else: 
+    print("Oops! That is not a valid input.")
