1d62cd317ae0a6cfea259eccf14399474914e761
Jkswm <beatrixawuah@outlook.com>
2023-11-01 13:50:16 +0000

    Updates milestone_4.py with word_guessed

--- milestone_4.py
+++ milestone_4.py
@@ -2,18 +2,23 @@ import random
 
 
 class Hangman:
-    def init(self, word_list, num_lives=5):
+    def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
         self.random_word = random.choice(word_list)
+        self.word_guessed = ['_'] * len(self.random_word)
         
 
         
 # Create an instance of the Hangman class
-word_list = ["grapes", "strawberries", "apples", "pears", "kiwis"]
+list = ["grapes", "strawberries", "apples", "pears", "kiwis"]
 
-hangman_instance = Hangman()
+hangman_instance = Hangman(list)
 
 # Access the attributes using the instance
+print(list)
+print("num lives:", hangman_instance.num_lives)
+print("word list:", hangman_instance.word_list)
+print("random word:", hangman_instance.random_word)
 
-print("word list:", hangman_instance)
+
