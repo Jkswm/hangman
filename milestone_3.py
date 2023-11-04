b7298e8f9f3919396b863c10d202245d929077b6
Jkswm <beatrixawuah@outlook.com>
2023-11-04 10:45:12 +0000

    Updates to milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -6,31 +6,23 @@ word_list = ["grapes", "strawberries", "apples", "pears", "kiwis"]
 word = random.choice(word_list)
 
 
-
-def ask_for_input(word):
+def ask_for_input():
     while True:
-        guess = input("Guess a letter: ").lower()
+        guess = input("Guess a letter: ")
+
         if len(guess) == 1 and guess.isalpha():
-            if check_guess(guess,word):
-                print("Yes")
+            break
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
+    
+    return guess
 
-
-def check_guess(guess,word):
+def check_guess(word, guess):
     if guess in word:
-            print("You guessed", guess)
-            print("Good guess! '{}' is in the word".format(guess))
-            return True
+        print(f"Good guess! {guess} is in the word.")
     else:
-        print(f"Sorry, '{guess}' is not in the word. Try again.")
-        return False
-   
-        
-        
-# Step 4: Call ask_for_input function to test the code
-ask_for_input(word)
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
-guess = input("Guess a letter: ").lower()
+user_input = ask_for_input()
 
-print(word)
+check_guess(word, user_input)
